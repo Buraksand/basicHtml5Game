7fd4ef1fe056160502c80c804c1a6661d3a1cae5
burak_sandiraz <buraksand@gmail.com>
2017-07-23 15:50:10 +0300

    small bug fixes

--- game.js
+++ game.js
@@ -1,16 +1,21 @@
 function startGame() {
   myGameArea.start();
+  showPerson();
 }
 
-var myGameArea = {
-  canvas: document.createElement("canvas"),
-  start: function() {
-    this.canvas.width = 300;
-    this.canvas.height = 150;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-  }
-}
+var myGameArea;
+myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        this.context.moveTo(0,0);
+        this.context.lineTo(480,270);
+        this.context.stroke();
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+};
 
 function showPerson() {
   var person = {
@@ -19,7 +24,7 @@ function showPerson() {
     age: 50,
     showFullName: function() {
       return "Age of " + person.name + " " + person.surname + " is " + person.age;
-    };
-  }
+    }
+  };
   document.getElementById("demo").innerHTML = person.showFullName();
 }
